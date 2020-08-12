6cf93a5ffccb261c6f2402621e928841fcf83af4
yuto-ts <kanon3749@gmail.com>
2020-08-12 23:43:27 +0900

    add pyenv go

--- TSUCHIYA-SETUP-MACHINE.sh
+++ TSUCHIYA-SETUP-MACHINE.sh
@@ -114,30 +114,76 @@ sudo apt-get install code # or code-insiders
 ## install steam in command line 
 sudo add-apt-repository multiverse
 sudo apt-get update
-sudo apt install steam
+sudo apt-get install steam
 
 ## install applications 
 sudo apt-get update
 sudo apt-get install -y \
-    terminator
+    terminator \
     vlc \
     emacs-nox \
-    gnome-tweaks \
+    gnome-tweaks 
 
-sudo apt update
-sudo apt install -y \
+sudo apt-get update
+sudo apt-get install -y \
     inkscape \
-    corebird \ # twitter cliant
-    evolution \ # mail and schduler
-    brasero \  # write tool CD/DVD 
-    rhythmbox-plugin-cdrecorder \ # copy music CD 
-    easytag \ # easy edit music tags 
-    clamtk \ # security software
-    gparted gpart \ # tool of manege partition 
-    install wine64 \ # wine
+    corebird \
+    evolution \
+    brasero \
+    rhythmbox-plugin-cdrecorder \
+    easytag \
+    clamtk \
+    gparted gpart \
+    install wine64  
 
 # using format type exfat]
 sudo apt-get install exfat-fuse exfat-utils
+##################################################################################
+
+# create Symbolic link
+ln -s /mnt/c/Users/yuto-tsu/prog ~
+
+##################################################################################
+
+# install python on pyenv
+# https://qiita.com/hasepyon/items/45ea509c64a6469ca5b6
+sudo apt-get update
+
+sudo apt-get install -y \
+    build-essential \
+    libffi-dev \
+    libssl-dev \
+    zlib1g-dev \
+    liblzma-dev \
+    libbz2-dev libreadline-dev libsqlite3-dev
+
+git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
+echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(pyenv init -)"' >> ~/.bashrc
+source ~/.bashrc
+# check pyenv installaion
+pyenv --version
+# pyenv install --list
+
+# install pip
+sudo apt-get update
+sudo apt-get install -y python3-pip
+sudo pip install --upgrade pip
+sudo pip3 install --upgrade pip
+
+
+##################################################################################
+
+# install golaung
+# https://qiita.com/notchi/items/5f76b2f77cff39eca4d8
+# https://golang.org/dl/
+wget https://golang.org/dl/go1.15.linux-amd64.tar.gz
+sudo tar -C /usr/local -xzf go1.15.linux-amd64.tar.gz
+export PATH=$PATH:/usr/local/go/bin
+# check go installaion
+go version
+
 
 ##################################################################################
 
@@ -155,6 +201,47 @@ fisher add 0rax/fish-bd
 
 ##################################################################################
 
+## nvidia-docker on WSL2
+# https://docs.nvidia.com/cuda/wsl-user-guide/index.html#installing-wsl2
+# requrement linux kernel 4.19.121+ is installed and Ensure that you install Build version 20145 or higher.
+# You can check your build version number by running winver via the Windows Run command.
+# check the version number by running the following command in PowerShell:
+# $ wsl cat /proc/version
 
+# Setting up CUDA Toolkit
 
-    
+# It is recommended to use the Linux package manager to install the CUDA for the Linux distributions supported under WSL 2. Follow these instructions to install the CUDA Toolkit.
+# First, set up the CUDA network repository. The instructions shown here are for Ubuntu 18.04. See the CUDA Linux Installation Guide for more information on other distributions.
+sudo apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub
+sudo sh -c 'echo "deb http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64 /" > /etc/apt/sources.list.d/cuda.list'
+
+# Now install CUDA. Note that for WSL 2, you should use the cuda-toolkit-<version> meta-package to avoid installing the NVIDIA driver that is typically bundled with the toolkit. You can also install other components of the toolkit by choosing the right meta-package.(https://docs.nvidia.com/cuda/cuda-installation-guide-linux/index.html#package-manager-metas)
+# Do not choose the cuda, cuda-11-0, or cuda-drivers meta-packages under WSL 2 since these packages will result in an attempt to install the Linux NVIDIA driver under WSL 2.
+sudo apt-get update
+sudo apt-get install -y cuda-toolkit-11-0
+
+# Install NVIDIA Container Toolkit
+# Now install the NVIDIA Container Toolkit (previously known as nvidia-docker2). WSL 2 support is available starting with nvidia-docker2 v2.3 and the underlying runtime library (libnvidia-container >= 1.2.0-rc.1).
+# For brevity, the installation instructions provided here are for Ubuntu 18.04 LTS.
+# Setup the stable and experimental repositories and the GPG key. The changes to the runtime to support WSL 2 are available in the experimental repository.
+distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
+curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -
+curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
+curl -s -L https://nvidia.github.io/libnvidia-container/experimental/$distribution/libnvidia-container-experimental.list | sudo tee /etc/apt/sources.list.d/libnvidia-container-experimental.lis
+
+# Install the NVIDIA runtime packages (and their dependencies) after updating the package listing.
+sudo apt-get update
+sudo apt-get install -y nvidia-docker2
+
+# Open a separate WSL 2 window and start the Docker daemon again using the following commands to complete the installation.
+sudo service docker stop
+sudo service docker start
+
+echo "alias docker_stop='sudo service docker stop'" >> ~/.bashrc
+echo "alias docker_start='sudo service docker start'" >> ~/.bashrc
+source ~/.bashrc
+
+##### Running CUDA Containers
+# Simple CUDA Containers
+# docker run --gpus all nvcr.io/nvidia/k8s/cuda-sample:nbody nbody -gpu -benchmark
+# docker run -it --gpus all -p 8888:8888 tensorflow/tensorflow:latest-gpu-py3-jupyter
